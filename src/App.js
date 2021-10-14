23d1f70e1f5d7da584f5ada34dd4a1e407be6be1
Alan Ren <10086000+alanvww@users.noreply.github.com>
2021-10-14 09:23:33 -0400

    Cleanup #2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './containers/Home';
 
 function App() {
